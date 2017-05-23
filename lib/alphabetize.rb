5b0813ba5176952b1f3db37834ce81046fdd8eb8
Joe Teichman <kaalm1@gmail.com>
2017-05-23 06:15:44 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,25 @@
+def transform(word)
+obj = {
+  'ĉ' => 'c',
+  'ĝ' => 'g',
+  'ĥ' => 'h',
+  'ĵ' => 'j',
+  'ŝ' => 's',
+  'ŭ' => 'u'
+}
+
+str = ''
+word.downcase.chars do |x|
+  if not obj[x].nil?
+    str += obj[x]
+  else
+    str += x
+  end
+end
+  str
+end
+
 def alphabetize(arr)
   # code here
+  arr.sort! { |a,b| transform(a) <=> transform(b)} 
 end
